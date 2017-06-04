c3f9da8aa5c00d36905bdf2a2e0fba0e577ea5f0
ysl4song <ysl4song@gmail.com>
2017-06-04 14:09:30 +0800

    Add files via upload

---
+++ q1.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int input = 14;
+	int count = 0;
+
+	do {
+		if (input & 0x01)
+			count++;
+
+		input >>= 1;
+	} while(input > 0);
+
+	printf("the number of 1 bits: %d", count);
+
+	return 0;
+}
